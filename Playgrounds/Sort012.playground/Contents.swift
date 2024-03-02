deabe9498e689df7e1a26d12b612c4121e321ebe
saanica16 <saanica16@gmail.com>
2024-03-03 01:24:08 +0530

    add sort 012

---
+++ Playgrounds/Sort012.playground/Contents.swift
@@ -0,0 +1,39 @@
+// Sort 0 1 2
+// https://leetcode.com/problems/sort-colors/description/
+
+/*
+ Input: nums = [2,0,2,1,1,0]
+ Output: [0,0,1,1,2,2]
+ */
+
+func sortColors(_ nums: inout [Int]) {
+    var low = 0
+    var mid = 0
+    var high = nums.count - 1
+    
+    while mid <= high {
+        if nums[mid] == 0 && low <= high {
+            let temp = nums[low]
+            nums[low] = nums[mid]
+            nums[mid] = temp
+            
+            low += 1
+            mid += 1
+        } else if nums[mid] == 1 && low <= high {
+            mid += 1
+        } else {
+            if low <= high {
+                let temp = nums[mid]
+                nums[mid] = nums[high]
+                nums[high] = temp
+                
+                high -= 1
+            }
+        }
+    }
+}
+
+//output
+var testArray = [2,0,2,1,1,0,0]
+sortColors(&testArray)
+print(testArray)
